8f9e3efe5028049fc5b934365f6ca7a05eb9bab7
alberto camilo <svg.z32@gmail.com>
2021-03-03 14:12:39 -0500

    Game2 refactoring

--- game-02/app/gilded-rose.ts
+++ game-02/app/gilded-rose.ts
@@ -10,50 +10,71 @@ export class Item {
     }
 }
 
-export class GildedRose {
+export interface IGildedRose {
+    decreaseQuality(item: Item, quantity: number): number
+
+    increaseQuality(item: Item, quantity: number): number
+
+    setQuality(item: Item, value): number
+}
+
+export abstract class AGildedRose implements IGildedRose {
+
+    decreaseQuality(item: Item, quantity = 1) {
+        return item.quality -= quantity;
+    }
+
+    increaseQuality(item: Item, quantity = 1) {
+        return item.quality += quantity;
+    }
+
+    setQuality(item: Item, value) {
+        return item.quality = value;
+    }
+
+}
+
+export class GildedRose extends AGildedRose {
     items: Array<Item>;
-    MAX_QUALITY = 50;
     MIN_QUALITY = 0;
+    MAX_QUALITY = 50;
 
     constructor(items = [] as Array<Item>) {
+        super();
         this.items = items;
     }
 
     updateQuality() {
-        this.items.forEach((item) => {
-                if (item.name == 'Sulfuras, Hand of Ragnaros')
-                    item.quality = 80
-                else if (item.name == 'Aged Brie')
-                    this.increaseQuality(item, 1)
-                else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if (item.sellIn <= 10)
-                        this.increaseQuality(item, 2)
-                    else if (item.sellIn <= 5)
-                        this.increaseQuality(item, 3)
-                    else if (item.sellIn <= 0)
-                        this.setQuality(item, 0);
-                    else this.increaseQuality(item, 1)
-                } else if (item.name == "Conjured Mana Cake")
-                    this.decreaseQuality(item, 2)
+        this.items.forEach((item) => this.checkQuality(item));
+    }
+
+    checkQuality(item: Item) {
+        switch (item.name) {
+            case 'Sulfuras, Hand of Ragnaros':
+                this.setQuality(item, 80);
+                break;
+            case 'Aged Brie':
+                this.increaseQuality(item)
+                break;
+            case 'Backstage passes to a TAFKAL80ETC concert':
+                if (item.sellIn <= 10)
+                    this.increaseQuality(item, 2)
+                else if (item.sellIn <= 5)
+                    this.increaseQuality(item, 3)
                 else if (item.sellIn < 0)
+                    this.setQuality(item, 0);
+                break;
+            case "Conjured Mana Cake":
+                this.decreaseQuality(item, 2)
+                break;
+            default:
+                if (item.sellIn < 0)
                     this.decreaseQuality(item, 2)
                 else
-                    item.quality >= this.MIN_QUALITY && item.quality <= this.MAX_QUALITY ? this.decreaseQuality(item, 1) : false;
-            }
-        );
-        return this.items;
-    }
+                    item.quality >= this.MIN_QUALITY && item.quality <= this.MAX_QUALITY ? this.decreaseQuality(item) : false;
+                break;
 
-    decreaseQuality(item: Item, quantity) {
-        item.quality -= quantity;
-    }
-
-    increaseQuality(item: Item, quantity) {
-        item.quality += quantity;
-    }
-
-    setQuality(item: Item, value) {
-        item.quality = value;
+        }
     }
 
 }
